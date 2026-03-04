acfe8bb038ace5c636a68c530eeccd99efa03b27
Aiman Fazal <twistofaiman@gmail.com>
2026-03-05 01:37:39 +0530

    Time: 0 ms (100.00%) | Memory: 8.4 MB (49.75%) - LeetSync

--- 2556-convert-the-temperature/convert-the-temperature.cpp
+++ 2556-convert-the-temperature/convert-the-temperature.cpp
@@ -3,11 +3,11 @@ public:
     vector<double> convertTemperature(double celcius) {
         vector<double> ans;
 
-        double k = celcius + 273.15;
-        double f = celcius * 1.80 + 32.00;
+        // double k = celcius + 273.15;
+        // double f = celcius * 1.80 + 32.00;
 
-        ans.push_back(k);
-        ans.push_back(f);
+        ans.push_back(celcius + 273.15);
+        ans.push_back(celcius * 1.80 + 32.00);
 
         return ans;
     }
