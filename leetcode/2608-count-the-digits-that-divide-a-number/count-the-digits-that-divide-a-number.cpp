ca3ffc2c4b0fda254c8d8e0ad8f2b0efda9501e0
Aiman Fazal <twistofaiman@gmail.com>
2026-05-18 12:31:41 +0530

    Time: 0 ms (100.00%) | Memory: 7.7 MB (73.36%) - LeetSync

--- leetcode/2608-count-the-digits-that-divide-a-number/count-the-digits-that-divide-a-number.cpp
+++ leetcode/2608-count-the-digits-that-divide-a-number/count-the-digits-that-divide-a-number.cpp
@@ -1,14 +1,13 @@
 class Solution {
 public:
     int countDigits(int num) {
-        int count = 0, digit, test = num;
+        int count = 0, test = num;
 
         while (test > 0) {
-            digit = test % 10;
-            test = test / 10;
-
-            if (num % digit == 0)
+            if (num % (test % 10) == 0)
                 count ++;
+
+            test = test / 10;
         }
 
         return count;
