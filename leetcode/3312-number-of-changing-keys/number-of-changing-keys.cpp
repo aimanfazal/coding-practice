06708ad3f1acb0e226e3962583111ee0b20010e9
Aiman Fazal <twistofaiman@gmail.com>
2026-05-21 16:47:39 +0530

    Time: 1 ms (14.56%) | Memory: 9.2 MB (15.27%) - LeetSync

---
+++ leetcode/3312-number-of-changing-keys/number-of-changing-keys.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int countKeyChanges(string s) {
+        int count = 0;
+
+        for (int i = 1; i < s.length(); i++) {
+            if ( (s[i] == s[i-1] || s[i] == s[i-1] - 32 || s[i] == s[i-1] + 32) )
+                continue;
+            else
+                count++;
+        }
+
+        return count;
+    }
+};
