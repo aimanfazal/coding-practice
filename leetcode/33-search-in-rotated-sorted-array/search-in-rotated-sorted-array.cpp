ff2c767049dd6c144018841f284e03d206e74fe5
Aiman Fazal <twistofaiman@gmail.com>
2026-05-22 15:12:01 +0530

    Time: 0 ms (100.00%) | Memory: 15 MB (95.73%) - LeetSync

--- leetcode/33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
+++ leetcode/33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
@@ -1,10 +1,16 @@
 class Solution {
 public:
     int search(vector<int>& nums, int target) {
-        for (int i = 0; i < nums.size(); i++)
+        int i = 0, j = nums.size() - 1;
+
+        while (i < nums.size() && j >= 0) {
             if (nums[i] == target)
                 return i;
-    
+            if (nums[j] == target)
+                return j;
+            i++;
+            j--;
+        }
     return -1;
     }
 };
