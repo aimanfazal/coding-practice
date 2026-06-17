dfbb6cb7c327914ade4d0a2a83b9173107a2b214
Aiman Fazal <twistofaiman@gmail.com>
2026-06-17 19:36:03 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Butterfly Pattern/butterfly-pattern.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int n;
+	cin>> n;
+	
+	// code here
+	for (int i = 1; i < n; i++) {
+		
+		for (int j = 1; j <= i; j++)
+			cout << "*";
+		
+		for (int j = 2 * (n - i) - 1; j >0; j--)
+			cout << " ";
+		
+		for (int j = 1; j <= i; j++)
+			cout << "*";
+		
+		cout << endl;
+	}
+	
+	for (int i = 0; i < 2*n - 1; i++)
+		cout << "*";
+	cout << endl;
+	
+	for (int i = 1; i < n; i++) {
+		
+		for (int j = n - i; j > 0; j--)
+			cout << "*";
+		
+		for (int j = 2*i - 1; j>0; j--)
+			cout << " ";
+		
+		for (int j = n - i; j > 0; j--)
+			cout << "*";
+			
+		cout << endl;
+	}
+	
+	return 0;
+}
