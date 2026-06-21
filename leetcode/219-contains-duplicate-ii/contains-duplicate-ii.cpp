22d22a968e6fd78cd20e95d43b544ee8eed6a1fa
Aiman Fazal <twistofaiman@gmail.com>
2026-06-21 22:04:28 +0530

    Time: 75 ms (55.66%) | Memory: 93.8 MB (79.82%) - LeetSync

--- leetcode/219-contains-duplicate-ii/contains-duplicate-ii.cpp
+++ leetcode/219-contains-duplicate-ii/contains-duplicate-ii.cpp
@@ -1,12 +1,18 @@
 class Solution {
 public:
     bool containsNearbyDuplicate(vector<int>& nums, int k) {
-        
+        unordered_set<int> window;
+
         for (int i = 0; i < nums.size(); i++) {
-            for (int j = i + 1; j <= (i + k) && j < nums.size(); j++)
-                if (nums[i] == nums[j])
-                    return true;
+            if (window.count(nums[i]))
+                return true;
+
+            window.insert(nums[i]);
+
+            if(window.size() > k)
+                window.erase(nums[i - k]);
         }
+
         return false;
     }
 };
