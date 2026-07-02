01a90bb3451a825b354385b936b789c9c5928453
Aiman Fazal <voidaiman.dev@gmail.com>
2026-07-02 23:56:27 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Max Triplet Sum/max-triplet-sum.cpp
@@ -0,0 +1,9 @@
+class Solution {
+  public:
+    int maxTripletSum(vector<int> &arr) {
+        // Code Here
+        sort(arr.rbegin(), arr.rend());
+        
+        return arr[0] + arr[1] + arr[2];
+    }
+};
