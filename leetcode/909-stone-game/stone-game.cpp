7195cf20099d0ea008738269db46d7c08ae5b11d
Aiman Fazal <voidaiman.dev@gmail.com>
2026-08-02 13:02:45 +0530

    Time: 19 ms (14.03%) | Memory: 19.6 MB (27.79%) - LeetSync

---
+++ leetcode/909-stone-game/stone-game.cpp
@@ -0,0 +1,24 @@
+class Solution {
+
+public:
+    vector<vector<int>> dp;
+
+    bool stoneGame(vector<int>& piles) {
+        dp.assign(piles.size(), vector<int>(piles.size(), INT_MIN));
+        return solution(piles, 0, piles.size() - 1) > 0;
+    }
+
+private:
+    int solution(vector<int>& data, int l, int r) {
+        if (l == r)
+            return data[l];
+
+        if (dp[l][r] != INT_MIN)
+            return dp[l][r];
+
+        int lc = data[l] - solution(data, l + 1, r);
+        int rc = data[r] - solution(data, l, r - 1);
+
+        return dp[l][r] = max(lc, rc);
+    }
+};
