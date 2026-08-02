854c663f47e7c22c9f23d39f6b5197201334d565
Aiman Fazal <voidaiman.dev@gmail.com>
2026-08-02 13:06:16 +0530

    Time: 0 ms (100.00%) | Memory: 10.5 MB (68.24%) - LeetSync

--- leetcode/909-stone-game/stone-game.cpp
+++ leetcode/909-stone-game/stone-game.cpp
@@ -1,24 +1,26 @@
 class Solution {
 
 public:
-    vector<vector<int>> dp;
+    // vector<vector<int>> dp;
 
     bool stoneGame(vector<int>& piles) {
-        dp.assign(piles.size(), vector<int>(piles.size(), INT_MIN));
-        return solution(piles, 0, piles.size() - 1) > 0;
+        // dp.assign(piles.size(), vector<int>(piles.size(), INT_MIN));
+        // return solution(piles, 0, piles.size() - 1) > 0;
+
+        return true;
     }
 
-private:
-    int solution(vector<int>& data, int l, int r) {
-        if (l == r)
-            return data[l];
+// private:
+//     int solution(vector<int>& data, int l, int r) {
+//         if (l == r)
+//             return data[l];
 
-        if (dp[l][r] != INT_MIN)
-            return dp[l][r];
+//         if (dp[l][r] != INT_MIN)
+//             return dp[l][r];
 
-        int lc = data[l] - solution(data, l + 1, r);
-        int rc = data[r] - solution(data, l, r - 1);
+//         int lc = data[l] - solution(data, l + 1, r);
+//         int rc = data[r] - solution(data, l, r - 1);
 
-        return dp[l][r] = max(lc, rc);
-    }
+//         return dp[l][r] = max(lc, rc);
+//     }
 };
