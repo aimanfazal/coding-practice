7c3030e12399a6690c18bb9a36562bd368384eb1
Aiman Fazal <voidaiman.dev@gmail.com>
2026-08-20 20:12:33 +0530

    Time: 0 ms (100.00%) | Memory: 23.7 MB (97.31%) - LeetSync

---
+++ leetcode/3347-distribute-elements-into-two-arrays-i/distribute-elements-into-two-arrays-i.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    vector<int> resultArray(vector<int>& nums) {
+
+        bool isSorted = true;
+
+        for (int i = 0; i < nums.size()-1; i++)
+            if (nums[i] > nums[i+1])
+                isSorted = false;
+
+        if (isSorted)
+            return nums;
+
+        vector<int> arr1, arr2;
+        arr1.push_back(nums[0]);
+        arr2.push_back(nums[1]);
+
+        for (int i = 2; i < nums.size(); i++)
+            if (arr1.back() > arr2.back())
+                arr1.push_back(nums[i]);
+            else
+                arr2.push_back(nums[i]);
+
+        for (int i = 0; i < arr2.size(); i++)
+            arr1.push_back(arr2[i]);
+
+        return arr1;
+    }
+};
